be13447c029f2fd32fc9fee99206fe13fa588e2a
darbean <wubing7137@gmail.com>
2014-10-07 17:53:45 +0800

    Initial Commit

---
+++ myFirstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myFirstApp
+//
+//  Created by darbean on 10/7/14.
+//  Copyright (c) 2014 darbean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myFirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myFirstApp
+//
+//  Created by darbean on 10/7/14.
+//  Copyright (c) 2014 darbean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
